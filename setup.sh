5d9978fbd19851c787b20e068e14708fb086ddce
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-09-25 18:03:35 +0800

    Install solarized theme.

--- setup.sh
+++ setup.sh
@@ -13,5 +13,8 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 # Download and install NERDTREE.
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+# Install vim-colors-solarized
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+
 # Create a hard symlink to user's .virmrc file.
 ln -n .vimrc ~/.vimrc
