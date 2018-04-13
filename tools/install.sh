a5f91af29ece1f3aa4eff1d7b260036956f69960
Emmanuel Corrales <contact@emmanuelcorrales.com>
2018-04-13 14:32:16 +0800

    Fix wrong directory for old dotfiles. Forgot to rename some variable BACKUP_DIR to DOTFILES_OLD.

--- tools/install.sh
+++ tools/install.sh
@@ -16,7 +16,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv $HOME/{} $BACKUP_DIR/{}
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv $HOME/{} $DOTFILES_OLD/{}
 fi;
 
 dotfiles checkout
