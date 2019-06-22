9653567d27fa67c97b649df4c83a1089b3a5d6ac
Emmanuel Corrales <contact@emcorrales.com>
2019-06-21 17:49:05 -0700

    Run ubuntu related script for Ubuntu only.

--- scripts/setup/docker.sh
+++ scripts/setup/docker.sh
@@ -9,23 +9,25 @@ if [ $(uname) = "Darwin" ]; then
   ln -s $etc/docker-machine.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-machine
   ln -s $etc/docker-compose.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-compose
 elif [ $(uname) = "Linux" ]; then
-  # SETUP REPOSITORY for Ubuntu.
-  sudo apt-get update
-  sudo apt-get install  apt-transport-https ca-certificates curl software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo apt-key fingerprint 0EBFCD88
-  sudo add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-     $(lsb_release -cs) stable"
+  if [ $(lsb_release -is) = "Ubuntu" ]; then
+    # SETUP REPOSITORY for Ubuntu.
+    sudo apt-get update
+    sudo apt-get install  apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) stable"
 
-  # INSTALL DOCKER
-  sudo apt-get update
-  sudo apt-get install docker-ce
+    # INSTALL DOCKER
+    sudo apt-get update
+    sudo apt-get install docker-ce
 
-  # Test docker installation.
-  sudo docker run hello-world
+    # Test docker installation.
+    sudo docker run hello-world
 
-  # Enable running docker without sudo.
-  sudo adduser $USER docker
-  echo "Please logout then login again to use docker without sudo."
+    # Enable running docker without sudo.
+    sudo adduser $USER docker
+    echo "Please logout then login again to use docker without sudo."
+  fi
 fi
