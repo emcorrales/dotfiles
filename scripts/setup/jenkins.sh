615dc7e820bdc2f2e6e2914d0e1f43f4b7dcfd3f
Emmanuel Corrales <contact@emcorrales.com>
2019-06-26 20:33:14 -0700

    Download and install JRE if not yet installed because it is needed.

--- scripts/setup/jenkins.sh
+++ scripts/setup/jenkins.sh
@@ -4,14 +4,14 @@ if [ $(uname) = "Linux" ]; then
 
   if [ ! $(java -version) ]; then
     echo "No JRE installed."
-    exit 1
+    sudo apt-get -y install default-jre
   fi
 
   if [ $(lsb_release -is) = "Ubuntu" ]; then
     wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
     sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
     sudo apt update
-    sudo apt install jenkins
+    sudo apt-get -y install jenkins
     sudo systemctl start jenkins
     sudo systemctl status jenkins
   else
