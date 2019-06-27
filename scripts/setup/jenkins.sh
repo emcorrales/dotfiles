ed7d1cb4d484c0e4037d15d0fe5ec382b374334f
Emmanuel Corrales <contact@emcorrales.com>
2019-06-26 18:36:56 -0700

    Support Jenkins installation for Ubuntu only.

--- scripts/setup/jenkins.sh
+++ scripts/setup/jenkins.sh
@@ -1,12 +1,28 @@
-#! /bin/bash
+#! /bin/bash -xe
 
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+if [ $(uname) = "Linux" ]; then
 
-# Opening the Firewall
-sudo ufw allow 8080
-sudo ufw status
+  if [ ! $(java -version) ]; then
+    echo "No JRE installed."
+    exit 1
+  fi
+
+  if [ $(lsb_release -is) = "Ubuntu" ]; then
+    wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+    sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt update
+    sudo apt install jenkins
+    sudo systemctl start jenkins
+    sudo systemctl status jenkins
+
+    # Opening the Firewall
+    sudo ufw allow 8080
+    sudo ufw status
+  else
+    echo "Unsupported linux distro."
+    exit 1
+  fi
+
+elif
+  exit 1
+fi
