025aeb0ad9fd48462783ba99a213db2644c4bfd0
Emmanuel Corrales <contact@emcorrales.com>
2019-06-26 17:46:40 -0700

    Create script for setting up jenkins.

---
+++ scripts/setup/jenkins.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+# Opening the Firewall
+sudo ufw allow 8080
+sudo ufw status
