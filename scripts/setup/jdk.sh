ab1763eabc5aee5e4508e80b80f59647a3fd77c6
Emmanuel Corrales <contact@emcorrales.com>
2019-06-26 18:08:05 -0700

    Exit if using Linux distro other that Ubuntu.

--- scripts/setup/jdk.sh
+++ scripts/setup/jdk.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if [ $(uname) = "Linux" ]; then
-  sudo add-apt-repository ppa:webupd8team/java
-  sudo apt-get update
+  if [ $(lsb_release -is) = "Ubuntu" ]; then
+    sudo add-apt-repository ppa:webupd8team/java
+    sudo apt-get update
 
-  sudo apt-get install oracle-java8-installer
-  sudo apt-get install oracle-java8-set-default
+    sudo apt-get install oracle-java8-installer
+    sudo apt-get install oracle-java8-set-default
+  else
+    echo "Unsupported linux distro."
+    exit 1
+  fi
 
 elif [ $(uname) = "Darwin" ]; then
   brew tap caskroom/versions
