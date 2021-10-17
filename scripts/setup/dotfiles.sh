0a9f386897697503ab360e506b20c12a8d01cfa6
Emmanuel Corrales <contact@emcorrales.com>
2021-10-18 04:46:11 +0800

    Replace ruby Homebrew installer with bash version.
    Ruby Homebrew installer has been deprecated and is replaced by the Bash version.

--- scripts/setup/dotfiles.sh
+++ scripts/setup/dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $(uname) = "Darwin" ]; then
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install tmux wget coreutils grep gnu-sed
 elif [ $(uname) = "Linux" ]; then
   sudo apt-get install build-essential curl tmux xclip
