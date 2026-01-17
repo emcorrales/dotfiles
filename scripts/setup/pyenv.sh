fb6766e25637c584c0972224e9fe6838ed591e2b
Emmanuel Corrales <contact@emcorrales.com>
2026-01-17 15:58:00 +0800

    Update pyenv setup script for MacOS.

--- scripts/setup/pyenv.sh
+++ scripts/setup/pyenv.sh
@@ -1,10 +1,10 @@
 #! /bin/bash +xe
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-exec "$SHELL"
 if [ $(uname) = "Linux" ]; then
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+  exec "$SHELL"
   if [ $(lsb_release -is) = "Ubuntu" ]; then
     sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
   elif [ $(lsb_release -is) = "CentOS" ]; then
@@ -15,10 +15,10 @@ if [ $(uname) = "Linux" ]; then
   fi
 
 elif [ $(uname) = "Darwin" ]; then
-  brew install openssl readline sqlite3 xz zlib
-  sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+  brew update
+  brew install pyenv
 else
   echo "OS not supported."
   exit 0
 fi
-pyenv install 2.7.8
+pyenv install 3.9.6
