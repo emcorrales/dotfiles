9cabf9359f8d15533284daa8949365ca69cc1bc4
Emmanuel M. Corrales <contact@emcorrales.com>
2026-04-03 20:30:45 +0800

    Install build dependecies and set the path for pyenv before installation.

--- scripts/setup/python-pyenv.sh
+++ scripts/setup/python-pyenv.sh
@@ -1,7 +1,16 @@
 #! /bin/bash +xe
 
 if [ $(uname) = "Linux" ]; then
+  sudo apt update
+  sudo apt install make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl git \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+
   curl -fsSL https://pyenv.run | bash
+
 elif [ $(uname) = "Darwin" ]; then
   brew update
   brew install pyenv
@@ -9,5 +18,8 @@ else
   echo "OS not supported."
   exit 0
 fi
-exec $SHELL
+
 pyenv install 3.14.3
+pyenv global 3.14.3
+exec $SHELL
+python --version
